7db7b9131298167b42158094ee1beb3cee05f173
Ashok2701 <i.ashokkumar1411@gmail.com>
2025-06-22 11:36:09 +0530

    Fix the code into latest changes 10

--- components/product/ProductForm.tsx
+++ components/product/ProductForm.tsx
@@ -72,7 +72,8 @@ export default function ProductForm({ sellerId }: { sellerId: string }) {
       return;
     }
 
-    const res = await fetch("/api/products", {
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
